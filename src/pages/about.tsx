800e3174bae8dd63f691c346f4053b46859a31b6
Adeilton Sousa <adeilton.sousa@climatempo.com.br>
2021-03-20 13:39:51 -0300

    Criando o método de getStaticPaths e fallback para um

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,37 @@
-import AboutTemplate from 'templates/About'
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
+import { useRouter } from 'next/dist/client/router'
+import PageTemplate from 'templates/About'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  const router = useRouter()
 
-export default About
+  if (router.isFallback) return null
+
+  return <PageTemplate />
+}
+
+export async function getStaticPatchs() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return { paths, fallback: true }
+}
+
+// export const getStaticProps = async () => {
+//   const { pages } = await client.request(GET_PAGES)
+
+//   console.log(pages)
+
+//   return {
+//     props: {}
+//   }
+// }
+
+// getStaticPaths => Gerar as urls em build time
+// getStaticProps => serve para buscar os dados na página (props) - build time - estático
+// getServerSideProps => serve para buscar os dados na página (props) - runtime - toda requisição (bundle fica no server)
+// getInitialProps => (deprecate) serve para buscar os dados na página (props) - runtime - toda requisição () (bundle tb vem para o client)
